ebd7df3ed0d0db8578d6822a6c7eb591e9aae61a
WangXiaoxi <wangxiaoxinyist@gmail.com>
2015-04-07 18:10:39 +0800

    关于绘图的小改动
    关于绘图的小改动

--- Learn2015-04/L02DrawApi/Classes/HelloWorldScene.cpp
+++ Learn2015-04/L02DrawApi/Classes/HelloWorldScene.cpp
@@ -6,6 +6,19 @@ USING_NS_CC;
 
 using namespace cocostudio::timeline;
 
+class MyShape : DrawNode {
+    
+public:
+    virtual void draw(Renderer *renderer, const Mat4& transform, uint32_t flags){
+        drawLine(Vec2(0, 0), Vec2(100, 100), Color4F::RED);
+    }
+    virtual bool init(){
+        
+        return true;
+    }
+    CREATE_FUNC(MyShape);
+};
+
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
@@ -16,10 +29,13 @@ Scene* HelloWorld::createScene()
 
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
+    
     // return the scene
     return scene;
 }
+
+
 
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
@@ -30,10 +46,13 @@ bool HelloWorld::init()
     {
         return false;
     }
+    MyShape * myShape = MyShape::create();
+    addChild(myShape);
+//    auto rootNode = CSLoader::createNode("MainScene.csb");
+//
+//    addChild(rootNode);
     
-    auto rootNode = CSLoader::createNode("MainScene.csb");
-
-    addChild(rootNode);
-
+    
+    
     return true;
 }
